4fcb7b2b0ad491b2540449639d3ec67f101357f5
Dylanwastaken12 <142276112+Dylanwastaken12@users.noreply.github.com>
2025-07-29 22:51:32 +0000

    Remove tests

--- Tests/literate-palm-treeTests/literate_palm_treeTests.swift
+++
@@ -1,6 +0,0 @@
-import Testing
-@testable import literate_palm_tree
-
-@Test func example() async throws {
-    // Write your test here and use APIs like `#expect(...)` to check expected conditions.
-}
